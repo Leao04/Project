4a5c8b4cb03ff8805a2359ab731659eb5186b415
MarceloJunior <malvezijunior@gmail.com>
2020-10-17 16:21:19 -0300

    Header
    Foi feito o Header.

--- components/Header.vue
+++ components/Header.vue
@@ -1,7 +1,34 @@
 <template>
   <div>
     <div class="grid">
-      
+      <div class="logo">
+        <img class="logo" src="../assets/img/ign-logo.png" alt="Logo da IGN" height="100em" width="230em">
+      </div>
+      <div class="sony">
+            <nuxt-link to="/">Sony</nuxt-link>
+      </div>
+      <div class="microsoft">
+        <nuxt-link to="/">Microsoft</nuxt-link>
+      </div>
+      <div class="nintendo">
+        <nuxt-link to="/">Nintendo</nuxt-link>
+      </div>
+      <div class="computador">
+        <nuxt-link to="/">Computador</nuxt-link>
+      </div>
+      <div class="barradebusca">
+        <div>
+      <b-navbar type="light" >
+        <b-nav-form>
+          <b-form-input class="mr-sm-2" placeholder="Pesquisar"></b-form-input>
+          <b-button variant="outline-success" class="my-2 my-sm-0" type="submit">Buscar</b-button>
+        </b-nav-form>
+      </b-navbar>
+    </div>
+      </div>
+      <div class="login">
+          <b-button variant="outline-primary">Login</b-button>
+      </div>
     </div>
   </div>
 </template>
@@ -11,3 +38,7 @@ export default {
     name: "Header"
 }
 </script>
+
+<style>
+@import "../assets/css/estilo.scss";
+</style>
