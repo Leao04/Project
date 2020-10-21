0a499d09f667a3414412b3398dea4f0bcdbcb50c
Leao04 <lordvergil12345@hotmail.com>
2020-10-20 21:32:37 -0300

    Footer; step one

---
+++ components/Footer.vue
@@ -0,0 +1,15 @@
+<template>
+  <div>
+      <div class="rodape"></div>
+  </div>
+</template>
+
+<script>
+export default {
+    name: "Footer"
+}
+</script>
+
+<style>
+@import "../assets/css/estilo.scss";
+</style>
