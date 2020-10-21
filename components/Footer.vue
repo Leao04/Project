f2905272120d721e126cf367c59854cebe195227
Leao04 <lordvergil12345@hotmail.com>
2020-10-20 21:53:49 -0300

    Footer

--- components/Footer.vue
+++ components/Footer.vue
@@ -4,6 +4,9 @@
           <div class="consoles">
               <nuxt-link to="/">Consoles</nuxt-link>
           </div>
+          <div class="xbox">
+            <nuxt-link to="/">Xbox One</nuxt-link>
+            </div>          
       </div>
   </div>
 </template>
