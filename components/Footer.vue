ae6a1dd4c5ff8d6a5518c0e57d334d02f2e4299e
Leao04 <lordvergil12345@hotmail.com>
2020-10-21 12:46:23 -0300

    Footer completed

--- components/Footer.vue
+++ components/Footer.vue
@@ -6,34 +6,92 @@
           </div>
           <div class="xbox">
             <nuxt-link to="/">Xbox One</nuxt-link>
+            <hr>
             </div>   
             <div class="play">
                 <nuxt-link to="/">Playstation 4</nuxt-link>
+                <hr>
             </div> 
             <div class="nintendo2">
                     <nuxt-link to="/">Wii U</nuxt-link>
+                    <hr>
             </div>
             <div class="nintendo3">
                 <nuxt-link to="/">Switch</nuxt-link>
+                <hr>
             </div>
             <div class="computador2">
                 <nuxt-link to="/">PC</nuxt-link>
+                <hr>
             </div>
             <div class="plays">
                 <nuxt-link to="/">Playstation 3</nuxt-link>
+                <hr>
             </div>
             <div class="xbox2">
                 <nuxt-link to="/">Xbox 360</nuxt-link>
+                <hr>
             </div>
             <div class="nintendo4">
                 <nuxt-link to="/">3DS</nuxt-link>
+                <hr>
             </div>
             <div class="playss">
                 <nuxt-link to="/">PSVita</nuxt-link>
+                <hr>
             </div>
             <div class="mobile">
                 <nuxt-link to="/">Celular</nuxt-link>
+                <hr>
             </div>
+            <div class="social">
+                <nuxt-link to="/">Redes Sociais</nuxt-link>
+            </div>
+            <div class="facebook">
+                <nuxt-link to="/">Facebook</nuxt-link>
+                <hr>
+            </div>
+            <div class="instagram">
+                <nuxt-link to="/">Instagram</nuxt-link>
+                <hr>
+            </div>
+            <div class="editoriais">
+                <p>Editoriais</p>
+            </div>
+            <div class="animes">
+                <nuxt-link to="/">Animes</nuxt-link>
+                <hr>
+            </div>
+            <div class="tv">
+                <nuxt-link to="/">TV</nuxt-link>
+                <hr>
+            </div>
+            <div class="previews">
+                <nuxt-link to="/">Previews</nuxt-link>
+                <hr>
+            </div>
+            <div class="cinema">
+                <nuxt-link to="/">Cinema</nuxt-link>
+                <hr>
+            </div>
+            <div class="ign">
+             <p>IGN Brasil</p>
+            </div>
+            <div class="work">
+                <nuxt-link to="/">Trabalhe conosco</nuxt-link>
+                <hr>
+            </div>
+            <div class="speak">
+                <nuxt-link to="/">Fale conosco</nuxt-link>
+                <hr>
+            </div>
+            <div class="about">
+                <nuxt-link to="/">Sobre nós</nuxt-link>
+                <hr>
+            </div>
+      </div>
+      <div class="final">
+          <p>©FATEC - todos os direitos reservados</p>
       </div>
   </div>
 </template>
