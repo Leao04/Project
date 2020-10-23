b26cbc4cb81d6ddfd967f3d63b7bbdad5ae7cd1a
Leao04 <lordvergil12345@hotmail.com>
2020-10-22 21:37:11 -0300

    haha

--- pages/lançamentos.vue
+++ pages/lançamentos.vue
@@ -1,7 +1,26 @@
 <template>
 <div>
-    <div class="titulo2">
+    <hr>
+    <br>
+    <div class="tit">
     <p>Os lançamentos da semana para PS4, Xbox One, Switch e PC</p>
+    </div>
+    <div class="imglaucher">
+        <img src="../assets/img/tecnologia.jpg" alt="banner" height="400px" width="100%">
+    </div>
+    <div>
+        <li class="amnesia">
+            20 de outubro: Amnesia: Rebirth - PS4, PC
+        </li>
+        <p class="rebirth">
+Amnesia: Rebirth foi desenvolvido pela Frictional Games. Foi seu primeiro jogo Amnesia desde The Dark Descent em 2010 e seu primeiro jogo desde Soma em 2015. Durante o desenvolvimento, a equipe tentou identificar os pontos fortes e fracos de The Dark Descent e Soma, e decidiu que Rebirth manteria o foco de Soma em ter uma narrativa abrangente e a "jogabilidade de nível inferior" de Amnesia. A equipe optou por retornar à franquia Amnesia porque sentiram que isso encurtaria o ciclo de desenvolvimento do jogo, já que a equipe só precisa refinar os elementos estabelecidos da franquia sem ter a necessidade de começar de novo. A equipe também sentiu que o Amnesia poderia se expandir para muitas outras configurações, conforme indicado pelas notas do jogo nos jogos originais. Apesar disso, o jogo não é uma sequência direta de The Dark Descent. Os jogadores não precisam jogar o primeiro jogo para entender as histórias, mas o jogo teria muito mais conexões com o jogo original do que A Machine for Pigs, que foi desenvolvido externamente pela The Chinese Room.
+
+Segundo Thomas Grip, o diretor criativo do jogo, o jogo explora a ideia de "sobrevivência". Em vez de confiar em sustos de curto prazo, a equipe acreditava que o tema e a narrativa abrangentes do jogo deveriam ser o que assombra o jogador. A equipa escolheu um cenário de deserto como cenário principal do jogo, pois permite à equipa criar ambientes mais diversos e permite aos jogadores apreciar momentos de beleza e espetáculo, que podem então ser usados ​​para contrastar as áreas mais claustrofóbicas e a sensação de pavor o jogo espera entregar. [7] Esqueletos no Zahara serviram como uma grande fonte de inspiração para a equipe. [6] Aprendendo com suas experiências na criação de Soma, a história do jogo se desdobra conforme o jogador avança, ao contrário de The Descent, cuja história é entregue principalmente por meio de coleções baseadas em texto e se concentra inteiramente no passado.
+
+A Frictional Games anunciou o Rebirth em 6 de março de 2020. O jogo foi lançado para Windows, Linux e PlayStation 4 em 20 de outubro de 2020.</p>
+    </div>
+    <div>
+        <object data="https://www.youtube.com/watch?v=eWDzqUud3jU" width="560" height="315"></object>
     </div>
 </div>
 </template>
