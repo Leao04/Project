0687c8a3dbfb5d93724397335fbea5ab3670fe3f
MarceloJr006 <malvezijunior@gmail.com>
2020-10-25 20:04:03 -0300

    Microsoft

--- pages/microsoft.vue
+++ pages/microsoft.vue
@@ -3,111 +3,82 @@
     <div>
       <img
         src="../assets/img/xbox-controles.jpg"
-        img-w
-        img-height="300px"
-        alt="PlayStation 1, 2, 3 e 4"
+        alt=""
+        height="600px"
+        width="100%"
       />
     </div>
-    <div>
-      <nuxt-link to="/">
-        <b-card
-          overlay
-          img-src="https://cache.desktopnexus.com/thumbseg/443/443592-bigthumbnail.jpg"
-          img-height="600px"
-          img-alt="PlayStation 3"
-          text-variant="black"
-          title="PlayStation 3"
-        >
-          <b-card-text>
-            O PlayStation 3 (PS3) é um console de videogames desenvolvido pela
-            Sony Computer Entertainment. É o sucessor do PlayStation 2 e faz
-            parte da marca PlayStation de consoles. Foi lançado em 11 de
-            novembro de 2006, no Japão, 17 de novembro de 2006 na América do
-            Norte e em 23 de março de 2007 na Europa e Oceania.
-          </b-card-text>
-        </b-card>
-      </nuxt-link>
-    </div>
-    <div>
-      <nuxt-link to="/">
-        <b-card
-          overlay
-          img-src="https://blog.br.playstation.com/tachyon/sites/4/2019/01/17_PS4Pro.jpg?resize=1088,612&crop_strategy=smart"
-          img-alt="PlayStation 4"
-          text-variant="black"
-          title="PlayStation 4"
-        >
-          <b-card-text>
-            PlayStation 4 é um console de jogos da oitava geração.
-          </b-card-text>
-        </b-card>
-      </nuxt-link>
-    </div>
-    <div class="containerSony">
-        
-    <div class="cardsony1">
-        <nuxt-link to="/lançamentos">
-      <img src="https://sm.ign.com/t/ign_br/slotter/default/illust-crown-2x_1bwz.560.png" alt="" height="490px" width="300px">
-      <p>Os lançamentos da semana para PS4, Xbox One, Switch e PC</p>
-      </nuxt-link>
-    </div>
-    <div class="cardsony2">
-        <nuxt-link to="/genshin">
-      <img src="https://sm.ign.com/t/ign_br/slotter/default/amber-home_nqxt.560.png" alt="" height="490px" width="300px">
-      <p>Genshin Impact: Como baixar e jogar o JRPG gacha do momento</p>
-      </nuxt-link>
-    </div>
-    <div class="cardsony3">
-        <nuxt-link to="/">
-      <img src="https://sm.ign.com/t/ign_br/slotter/default/alien-isolation_vcc7.560.jpg" alt="" height="490px" width="300px">
-      <p>Games de PS4 e Xbox One com até 90% de desconto</p>
-      </nuxt-link>
-    </div>
 
-    <div class="cardsony4">
-        <nuxt-link to="/">
-      <img src="https://sm.ign.com/t/ign_es/slotter/default/monhun1_kezm.560.jpg" alt="" height="490px" width="300px">
-      <p>Como Monster Hunter World influenciou filme de caçador de ...</p>
-      </nuxt-link>
+    <div class="containerMicrosoft">
+      <div class="CardsJuntos">
+        <div>
+          <b-card no-body class="overflow-hidden" style="max-width: 540px">
+            <nuxt-link to="/">
+            <b-row no-gutters>
+              <b-col md="6">
+                <b-card-img
+                  src="https://assets1.ignimgs.com/2020/05/06/assassins-creed-valhalla-1588780126472.jpg?width=300&crop=16%3A9"
+                  alt="Image"
+                  class="rounded-0"
+                ></b-card-img>
+              </b-col>
+              <b-col md="6">
+                <b-card-body title="Horizontal Card">
+                  <b-card-text>
+                    Xbox Series X Lista de Jogos de Lançamento - Resoluções e Framerates
+                  </b-card-text>
+                </b-card-body>
+              </b-col>
+            </b-row>
+            </nuxt-link>
+          </b-card>
+        </div>
+        <div>
+          <b-card no-body class="overflow-hidden" style="max-width: 540px">
+            <nuxt-link to="/">
+            <b-row no-gutters>
+              <b-col md="6">
+                <b-card-img
+                  src="https://assets1.ignimgs.com/2019/02/08/tipsandtrickspromo-1549594476063.jpg?width=300&crop=16%3A9"
+                  alt="Image"
+                  class="rounded-0"
+                ></b-card-img>
+              </b-col>
+              <b-col md="6">
+                <b-card-body title="Horizontal Card">
+                  <b-card-text>
+                    Versão do Switch do Apex Legends adiada para 2021
+                  </b-card-text>
+                </b-card-body>
+              </b-col>
+            </b-row>
+            </nuxt-link>
+          </b-card>
+        </div>
+        <div>
+          <b-card no-body class="overflow-hidden" style="max-width: 540px">
+            <nuxt-link to="/">
+            <b-row no-gutters>
+              <b-col md="6">
+                <b-card-img
+                  src="https://assets2.ignimgs.com/2014/11/13/halo-mccjpg-bc17eb.jpg?width=300&crop=16%3A9"
+                  alt="Image"
+                  class="rounded-0"
+                ></b-card-img>
+              </b-col>
+              <b-col md="6">
+                <b-card-body title="Horizontal Card">
+                  <b-card-text>
+                    Halo Movie: Guillermo del Toro aparentemente queria mestre chefe para <a href="http://">Ver Mais</a>
+                  </b-card-text>
+                </b-card-body>
+              </b-col>
+            </b-row>
+            </nuxt-link>
+          </b-card>
+        </div>
+      </div>
     </div>
-    <div class="cardsony5">
-        <nuxt-link to="/">
-      <img src="https://sm.ign.com/t/ign_es/slotter/default/apps52688139966321892364146b579efe-3833-4c8e-846c-120bf230ab_sy8n.560.jpg" alt="" height="490px" width="300px">
-      <p>Kingdom Hearts: Melody of Memory Final Prints para PS4, Xbox One</p>
-      </nuxt-link>
-    </div>
-    <div class="cardsony6">
-        <nuxt-link to="/">
-      <img src="https://sm.ign.com/t/ign_es/slotter/default/stw-squadrons_6ppg.560.jpg" alt="" height="490px" width="300px">
-      <p>Star Wars: Esquadrões - Análise</p>
-      </nuxt-link>
-    </div>
-    <div class="cardsony7">
-        <nuxt-link to="/">
-      <img src="https://sm.ign.com/t/ign_pt/screenshot/default/dt_u5hm.560.jpg" alt="" height="490px" width="300px">
-      <p>Detroit: Become Human é o que mais rapidamente vendeu da Quantic Dream</p>
-      </nuxt-link>
-    </div>
-    <div class="cardsony8">
-        <nuxt-link to="/">
-      <img src="https://sm.ign.com/t/ign_ap/screenshot/default/raya-and-the-last-dragon-trailer_aacr.560.jpg" alt="" height="490px" width="300px">
-      <p>Por que os asiáticos do sudeste devem estar animados com Raya ...</p>
-      </nuxt-link>
-    </div>
-    <div class="cardsony9">
-    <nuxt-link to="/">
-      <img src="https://sm.ign.com/t/ign_ap/screenshot/default/aniversary-16-9-7_jqc5.560.png" alt="" height="490px" width="300px">
-      <p>Sequência do premiado jogo tailandês 'Terra-Atlântida'...</p>
-    </nuxt-link>
-    </div>
-
-
-
-
-    </div>
-
-
-
   </div>
 </template>
 
