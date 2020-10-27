606be7c4ba586b0bf8585134c685fc7e1da91c5e
Leao04 <lordvergil12345@hotmail.com>
2020-10-26 22:16:49 -0300

    .

---
+++ pages/desconto.vue
@@ -0,0 +1,18 @@
+<template>
+  <div>
+    <div class="introduction">
+      <p>
+        Jogos de PlayStation 4 e Xbox One estão disponíveis com descontos de até
+        90% neste final semana — e os preços podem ser ainda menores se você for
+        assinante dos serviços PS Plus ou Xbox Live Gold. Entre os games com
+        desconto no PS4, chama atenção o valor de Devil May Cry 5, disponível
+        por R$ 83,12. Dos jogos em promoção na loja da Xbox, destaca-se Gears 5,
+        que custa R$ 99,50.
+      </p>
+    </div>
+  </div>
+</template>
+
+<style>
+@import "../assets/css/estilo.scss";
+</style>
--- pages/sony.vue
+++ pages/sony.vue
@@ -44,70 +44,114 @@
       </nuxt-link>
     </div>
     <div class="containerSony">
-        
-    <div class="cardsony1">
+      <div class="cardsony1">
         <nuxt-link to="/lançamentos">
-      <img src="https://sm.ign.com/t/ign_br/slotter/default/illust-crown-2x_1bwz.560.png" alt="" height="490px" width="300px">
-      <p>Os lançamentos da semana para PS4, Xbox One, Switch e PC</p>
-      </nuxt-link>
-    </div>
-    <div class="cardsony2">
+          <img
+            src="https://sm.ign.com/t/ign_br/slotter/default/illust-crown-2x_1bwz.560.png"
+            alt=""
+            height="490px"
+            width="300px"
+          />
+          <p>Os lançamentos da semana para PS4, Xbox One, Switch e PC</p>
+        </nuxt-link>
+      </div>
+      <div class="cardsony2">
         <nuxt-link to="/genshin">
-      <img src="https://sm.ign.com/t/ign_br/slotter/default/amber-home_nqxt.560.png" alt="" height="490px" width="300px">
-      <p>Genshin Impact: Como baixar e jogar o JRPG gacha do momento</p>
-      </nuxt-link>
-    </div>
-    <div class="cardsony3">
+          <img
+            src="https://sm.ign.com/t/ign_br/slotter/default/amber-home_nqxt.560.png"
+            alt=""
+            height="490px"
+            width="300px"
+          />
+          <p>Genshin Impact: Como baixar e jogar o JRPG gacha do momento</p>
+        </nuxt-link>
+      </div>
+      <div class="cardsony3">
+        <nuxt-link to="/desconto">
+          <img
+            src="https://sm.ign.com/t/ign_br/slotter/default/alien-isolation_vcc7.560.jpg"
+            alt=""
+            height="490px"
+            width="300px"
+          />
+          <p>Games de PS4 e Xbox One com até 90% de desconto</p>
+        </nuxt-link>
+      </div>
+
+      <div class="cardsony4">
         <nuxt-link to="/">
-      <img src="https://sm.ign.com/t/ign_br/slotter/default/alien-isolation_vcc7.560.jpg" alt="" height="490px" width="300px">
-      <p>Games de PS4 e Xbox One com até 90% de desconto</p>
-      </nuxt-link>
-    </div>
-
-    <div class="cardsony4">
+          <img
+            src="https://sm.ign.com/t/ign_es/slotter/default/monhun1_kezm.560.jpg"
+            alt=""
+            height="490px"
+            width="300px"
+          />
+          <p>Como Monster Hunter World influenciou filme de caçador de ...</p>
+        </nuxt-link>
+      </div>
+      <div class="cardsony5">
         <nuxt-link to="/">
-      <img src="https://sm.ign.com/t/ign_es/slotter/default/monhun1_kezm.560.jpg" alt="" height="490px" width="300px">
-      <p>Como Monster Hunter World influenciou filme de caçador de ...</p>
-      </nuxt-link>
-    </div>
-    <div class="cardsony5">
+          <img
+            src="https://sm.ign.com/t/ign_es/slotter/default/apps52688139966321892364146b579efe-3833-4c8e-846c-120bf230ab_sy8n.560.jpg"
+            alt=""
+            height="490px"
+            width="300px"
+          />
+          <p>
+            Kingdom Hearts: Melody of Memory Final Prints para PS4, Xbox One
+          </p>
+        </nuxt-link>
+      </div>
+      <div class="cardsony6">
         <nuxt-link to="/">
-      <img src="https://sm.ign.com/t/ign_es/slotter/default/apps52688139966321892364146b579efe-3833-4c8e-846c-120bf230ab_sy8n.560.jpg" alt="" height="490px" width="300px">
-      <p>Kingdom Hearts: Melody of Memory Final Prints para PS4, Xbox One</p>
-      </nuxt-link>
-    </div>
-    <div class="cardsony6">
+          <img
+            src="https://sm.ign.com/t/ign_es/slotter/default/stw-squadrons_6ppg.560.jpg"
+            alt=""
+            height="490px"
+            width="300px"
+          />
+          <p>Star Wars: Esquadrões - Análise</p>
+        </nuxt-link>
+      </div>
+      <div class="cardsony7">
         <nuxt-link to="/">
-      <img src="https://sm.ign.com/t/ign_es/slotter/default/stw-squadrons_6ppg.560.jpg" alt="" height="490px" width="300px">
-      <p>Star Wars: Esquadrões - Análise</p>
-      </nuxt-link>
-    </div>
-    <div class="cardsony7">
+          <img
+            src="https://sm.ign.com/t/ign_pt/screenshot/default/dt_u5hm.560.jpg"
+            alt=""
+            height="490px"
+            width="300px"
+          />
+          <p>
+            Detroit: Become Human é o que mais rapidamente vendeu da Quantic
+            Dream
+          </p>
+        </nuxt-link>
+      </div>
+      <div class="cardsony8">
         <nuxt-link to="/">
-      <img src="https://sm.ign.com/t/ign_pt/screenshot/default/dt_u5hm.560.jpg" alt="" height="490px" width="300px">
-      <p>Detroit: Become Human é o que mais rapidamente vendeu da Quantic Dream</p>
-      </nuxt-link>
-    </div>
-    <div class="cardsony8">
+          <img
+            src="https://sm.ign.com/t/ign_ap/screenshot/default/raya-and-the-last-dragon-trailer_aacr.560.jpg"
+            alt=""
+            height="490px"
+            width="300px"
+          />
+          <p>
+            Por que os asiáticos do sudeste devem estar animados com Raya ...
+          </p>
+        </nuxt-link>
+      </div>
+      <div class="cardsony9">
         <nuxt-link to="/">
-      <img src="https://sm.ign.com/t/ign_ap/screenshot/default/raya-and-the-last-dragon-trailer_aacr.560.jpg" alt="" height="490px" width="300px">
-      <p>Por que os asiáticos do sudeste devem estar animados com Raya ...</p>
-      </nuxt-link>
+          <img
+            src="https://sm.ign.com/t/ign_ap/screenshot/default/aniversary-16-9-7_jqc5.560.png"
+            alt=""
+            height="490px"
+            width="300px"
+          />
+          <p>Sequência do premiado jogo tailandês 'Terra-Atlântida'...</p>
+        </nuxt-link>
+      </div>
     </div>
-    <div class="cardsony9">
-    <nuxt-link to="/">
-      <img src="https://sm.ign.com/t/ign_ap/screenshot/default/aniversary-16-9-7_jqc5.560.png" alt="" height="490px" width="300px">
-      <p>Sequência do premiado jogo tailandês 'Terra-Atlântida'...</p>
-    </nuxt-link>
-    </div>
-
-
-
-
-    </div>
-
-
-
   </div>
 </template>
 
