c8bc86a3caaf896669ba20e8707b833c7d6c6b9c
ViniciusLanghi <vinicius_langhi@yahoo.com.br>
2020-10-28 12:08:13 -0300

    .
    .

--- .nuxt/router.js
+++ .nuxt/router.js
@@ -3,15 +3,15 @@ import Router from 'vue-router'
 import { interopDefault } from './utils'
 import scrollBehavior from './router.scrollBehavior.js'
 
-const _6393e6d1 = () => interopDefault(import('..\\pages\\criar-post.vue' /* webpackChunkName: "pages/criar-post" */))
-const _9eff2dcc = () => interopDefault(import('..\\pages\\desconto.vue' /* webpackChunkName: "pages/desconto" */))
-const _9f69fa2a = () => interopDefault(import('..\\pages\\genshin.vue' /* webpackChunkName: "pages/genshin" */))
-const _7fd51496 = () => interopDefault(import('..\\pages\\lançamentos.vue' /* webpackChunkName: "pages/lançamentos" */))
-const _426046d2 = () => interopDefault(import('..\\pages\\logado.vue' /* webpackChunkName: "pages/logado" */))
-const _febbf0ec = () => interopDefault(import('..\\pages\\login.vue' /* webpackChunkName: "pages/login" */))
-const _53945be2 = () => interopDefault(import('..\\pages\\microsoft.vue' /* webpackChunkName: "pages/microsoft" */))
-const _7204d5d4 = () => interopDefault(import('..\\pages\\sony.vue' /* webpackChunkName: "pages/sony" */))
-const _920b371a = () => interopDefault(import('..\\pages\\index.vue' /* webpackChunkName: "pages/index" */))
+const _90dda0f8 = () => interopDefault(import('..\\pages\\criar-post.vue' /* webpackChunkName: "pages/criar-post" */))
+const _5ee37ae6 = () => interopDefault(import('..\\pages\\desconto.vue' /* webpackChunkName: "pages/desconto" */))
+const _2d32af18 = () => interopDefault(import('..\\pages\\genshin.vue' /* webpackChunkName: "pages/genshin" */))
+const _a8fe3b7a = () => interopDefault(import('..\\pages\\lançamentos.vue' /* webpackChunkName: "pages/lançamentos" */))
+const _1949126c = () => interopDefault(import('..\\pages\\logado.vue' /* webpackChunkName: "pages/logado" */))
+const _15f0daf7 = () => interopDefault(import('..\\pages\\login.vue' /* webpackChunkName: "pages/login" */))
+const _9039b208 = () => interopDefault(import('..\\pages\\microsoft.vue' /* webpackChunkName: "pages/microsoft" */))
+const _dbffbfee = () => interopDefault(import('..\\pages\\sony.vue' /* webpackChunkName: "pages/sony" */))
+const _4c4937e0 = () => interopDefault(import('..\\pages\\index.vue' /* webpackChunkName: "pages/index" */))
 
 // TODO: remove in Nuxt 3
 const emptyFn = () => {}
@@ -31,39 +31,39 @@ export const routerOptions = {
 
   routes: [{
     path: "/criar-post",
-    component: _6393e6d1,
+    component: _90dda0f8,
     name: "criar-post"
   }, {
     path: "/desconto",
-    component: _9eff2dcc,
+    component: _5ee37ae6,
     name: "desconto"
   }, {
     path: "/genshin",
-    component: _9f69fa2a,
+    component: _2d32af18,
     name: "genshin"
   }, {
     path: "/lançamentos",
-    component: _7fd51496,
+    component: _a8fe3b7a,
     name: "lançamentos"
   }, {
     path: "/logado",
-    component: _426046d2,
+    component: _1949126c,
     name: "logado"
   }, {
     path: "/login",
-    component: _febbf0ec,
+    component: _15f0daf7,
     name: "login"
   }, {
     path: "/microsoft",
-    component: _53945be2,
+    component: _9039b208,
     name: "microsoft"
   }, {
     path: "/sony",
-    component: _7204d5d4,
+    component: _dbffbfee,
     name: "sony"
   }, {
     path: "/",
-    component: _920b371a,
+    component: _4c4937e0,
     name: "index"
   }],
 
