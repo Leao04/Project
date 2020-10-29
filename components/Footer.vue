bfb80aee613fb7c9e0a9a1ab051761dfba552b1e
MarceloJr006 <malvezijunior@gmail.com>
2020-10-28 23:31:34 -0300

    Arrumando o Footer

--- components/Footer.vue
+++ components/Footer.vue
@@ -5,43 +5,15 @@
         <nuxt-link to="/">Consoles</nuxt-link>
       </div>
       <div class="xbox">
-        <nuxt-link to="/">Xbox One</nuxt-link>
+        <nuxt-link to="/">Microsoft</nuxt-link>
         <hr />
       </div>
       <div class="play">
-        <nuxt-link to="/">Playstation 4</nuxt-link>
+        <nuxt-link to="/">Playstation</nuxt-link>
         <hr />
       </div>
       <div class="nintendo2">
-        <nuxt-link to="/">Wii U</nuxt-link>
-        <hr />
-      </div>
-      <div class="nintendo3">
-        <nuxt-link to="/">Switch</nuxt-link>
-        <hr />
-      </div>
-      <div class="computador2">
-        <nuxt-link to="/">PC</nuxt-link>
-        <hr />
-      </div>
-      <div class="plays">
-        <nuxt-link to="/">Playstation 3</nuxt-link>
-        <hr />
-      </div>
-      <div class="xbox2">
-        <nuxt-link to="/">Xbox 360</nuxt-link>
-        <hr />
-      </div>
-      <div class="nintendo4">
-        <nuxt-link to="/">3DS</nuxt-link>
-        <hr />
-      </div>
-      <div class="playss">
-        <nuxt-link to="/">PSVita</nuxt-link>
-        <hr />
-      </div>
-      <div class="mobile">
-        <nuxt-link to="/">Celular</nuxt-link>
+        <nuxt-link to="/">Nintendo</nuxt-link>
         <hr />
       </div>
       <div class="social">
