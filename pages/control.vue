12c0d1bbf0f53251c43a613135e8ac3fbe513f93
ViniciusLanghi <vinicius_langhi@yahoo.com.br>
2020-11-05 09:13:43 -0300

    paginacontrol

---
+++ pages/control.vue
@@ -0,0 +1,29 @@
+<template>
+  <div>
+    <div>
+      <p class="control">
+        Control: Ultimate Edition está disponível no Nintendo Switch
+      </p>
+    </div>
+    <div class="controlvideo" align="center">
+      <video controls="controls" src="../assets/video/control.mp4"></video>
+    </div>
+    <div class="conteudocontrol">
+     <p>O jogo Control: Ultimate Edition chegou em mais uma plataforma hoje (28):
+      no Nintendo Switch. <br>O game está disponível via streaming, o que significa
+      que o console não precisa processar os dados do jogo e que você não
+      precisa de muito espaço para ter o título. <br> Uma demonstração está
+      disponível na eShop gratuitamente. O download total é de 98 MB, e o
+      jogador tem acesso a um launcher que faz a conexão entre o Nintendo Switch
+      e os servidores onde o jogo está. Por ter sido anunciado ainda hoje em um
+      Direct mini, quem quiser
+      experimentar a versão terá que esperar um pouco, pois a capacidade dos
+      servidores é limitada e há uma fila para o teste.
+      </p>
+    </div>
+  </div>
+</template>
+
+<style>
+@import '../assets/css/estilo.scss';
+</style>
