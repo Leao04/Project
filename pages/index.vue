be0663d9124201dfc0e51979b05bdfa3998a3904
Leao04 <66676211+Leao04@users.noreply.github.com>
2020-11-15 23:26:23 -0300

    Restart

--- pages/index.vue
+++ pages/index.vue
@@ -175,23 +175,6 @@ export default {
   },
 }
 </script>
-//
-<script src="../dist/countdown-timer.js"></script>
-//
-<script>
-let countdownTimer = new TenUp.CountdownTimer('.countdown-timer', {
-  onCreate: function (instance) {
-    console.log('onCreate callback', instance)
-  },
-
-  onEnd: function (instance) {
-    console.log('onEnd callback', instance)
-  },
-  onTick: function (instance) {
-    console.log('onTick callback', instance)
-  },
-})
-</script>
 
 <style>
 @import '../assets/css/estilo.scss';
